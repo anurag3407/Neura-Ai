2ec7bcbc82d5b831aca17b72a88e91f1d25aca68
Anurag Mishra <anuragmishra3407@gmail.com>
2025-05-22 11:40:49 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-//  export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDMr-99c8cDS929GvMkMqZzqSYKQmjDMFo"
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDMr-99c8cDS929GvMkMqZzqSYKQmjDMFo"
 
-export const URL = import.meta.env.VITE_API_URL+import.meta.env.VITE_API_KEY
+// export const URL = import.meta.env.VITE_API_URL+import.meta.env.VITE_API_KEY
